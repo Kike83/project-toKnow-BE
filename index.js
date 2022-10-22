66080fe909b780f94135a0e58b0f7ff5bd1de287
KikeBA <enrique.berdichevsky@gmail.com>
2022-10-22 17:20:15 -0500

    add index

---
+++ index.js
@@ -0,0 +1,21 @@
+require('dotenv').config()
+
+const mongoose = require("mongoose")
+
+const server = require("./src/server")
+
+
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env
+
+
+mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`)
+.then(() => {                       
+    console.log("La BD está conectada")
+
+    server.listen(8080, (request, response) => {
+        console.log("El servidor está encendido");
+    })
+})
+.catch((err) => {
+    console.log("Error al conectar a la BD", err)
+})
