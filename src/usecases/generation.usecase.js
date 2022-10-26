9a873f52684706be92199ab5b30dfb775ca3aaec
disection <diseccionmexico@gmail.com>
2022-10-26 16:48:23 -0500

    ADD generation usecase get, getById, create, remove and update

---
+++ src/usecases/generation.usecase.js
@@ -0,0 +1,39 @@
+const Generations = require ("../models/generation.model.js")
+const createError = require ('http-errors')
+
+const getAll = () =>{
+    const generations = Generations.find({})
+    return generations
+}
+
+const getById = async (id) =>{
+    const generation = await Generations.findById(id)
+    
+    if(!generation){
+        const error = createError(404, "Generación no encontrada")
+        throw error
+    }
+
+    return generation
+}
+
+const create = async (data) =>{
+    const generation = await Generations.create(data)
+    return generation
+}
+
+const remove = async (id) => {
+    const generation = await Generations.findByIdAndDelete(id)
+    if (!school){
+        const error = createError(404, "Escuela no encontrada")
+        throw error
+    } 
+    return generation
+}
+
+const update = async (id,data) =>{
+    const generation = await Generations.findByIdAndUpdate(id, data, {returnDocument: "after"})
+    return generation
+}
+
+module.exports= {getAll, getById, create, update, remove}
