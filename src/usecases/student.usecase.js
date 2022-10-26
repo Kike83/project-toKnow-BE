00fff6f42b8b78729fab7d276d4de852a1907cc6
KikeBA <enrique.berdichevsky@gmail.com>
2022-10-25 21:18:29 -0500

    add student-crud, http_errors

---
+++ src/usecases/student.usecase.js
@@ -0,0 +1,55 @@
+const Student = require("../models/student.model")
+const createError = require('http-errors')
+
+
+
+// Usecase 1 - Get
+const getAll = () => {
+    console.log("imprimiendo desde student.usecase dentro de getAll")
+    const students = Student.find({})
+    return students
+}
+
+
+
+// Usecase 2 - GetById
+const getById = async (id) => {
+    console.log("imprimiendo desde student.usecase dentro de getByID")
+    const studentById = await Student.findById( id )
+    if(!studentById) {
+        const error = createError(404, "El student no fue encontrado")
+        throw error
+    }
+    return studentById
+}
+
+
+
+// Usecase 3 - Post
+const create = async (studentData) => {
+    console.log("imprimiendo desde student.usecase dentro del Post")
+    const studentToCreate = await Student.create( studentData )
+    return studentToCreate
+}
+
+
+
+// Usecase 4 - Patch
+const update = (id, studentData) => {
+    const studentToPatch = Student.findByIdAndUpdate(id, studentData, { returnDocument: 'after' })
+    return studentToPatch
+    }
+
+
+
+// Usecase 5 - Delete
+const remove = (id) => {
+    const studentToDelete = Student.findByIdAndDelete(id)
+    return studentToDelete
+    }
+
+
+
+
+
+module.exports = { getAll, getById, create, update, remove }
