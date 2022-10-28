c18564a8e0760eae8ba10dfa2e75479fc61187bc
KikeBA <enrique.berdichevsky@gmail.com>
2022-10-28 10:27:51 -0500

    fix server

--- src/server.js
+++ src/server.js
@@ -1,26 +1,23 @@
 const express = require("express");
 const app = express();
 const routerSchool = require("./routes/school.route")
+const routerGroup = require("./routes/group.route")
 const routerTeacher = require("./routes/teacher.route")
 const routerStudent = require("./routes/student.route")
 const routerUser = require("./routes/user.route")
 
-
 app.use(express.json())
 
-
 app.use("/school", routerSchool)
+app.use("/group", routerGroup)
 app.use("/teachers", routerTeacher)
 app.use("/students", routerStudent)
 app.use("/users", routerUser)
 
-
 app.get("/", (resquest, response) => {
-
     response.json({
     message: "Bienvenidos a toKnow. Endpoint de Home"
+    })
 })
-})
-
 
 module.exports = app
